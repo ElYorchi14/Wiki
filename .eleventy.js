464f5fed5653ce00f9661fb249f497508c1e8e96
Jorge Palacios <cdmx2637@amerike.edu.mx>
2024-06-03 20:43:00 -0600

    Arreglando search bar

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,16 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
-module.exports = function(eleventyConfig) {
+
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("code/css");
+  eleventyConfig.addPassthroughCopy("code/js");
+  eleventyConfig.addPassthroughCopy("code/img");
+
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-return {
-  
+
+  return {
     dir: {
-      
       input: "code",
       output: "docs",
-    }
+    },
   };
-}
+};
